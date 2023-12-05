c4fb255b2f698b7b06252eb099057819d4ebb934
WikingWarr10r <110036601+WikingWarr10r@users.noreply.github.com>
2023-12-05 21:32:19 +0000

    Level Editor Update: Deletion, encrypted storage and selection of walls

--- DoomStyleEngine/res/shaders/default.frag
+++ DoomStyleEngine/res/shaders/default.frag
@@ -1,10 +1,10 @@
 #version 330 core
 
-in vec2 TexCoord; // Input texture coordinates from vertex shader
+in vec2 TexCoord; 
 
 out vec4 colour;
 
 void main()
 {
-    colour = vec4(0.2, 0.2, 0.8, 1.0);
+    colour = vec4(0.2, 0.7, 0.2, 1.0);
 }
--- DoomStyleEngine/tools/scripts/level_editor.py
+++ DoomStyleEngine/tools/scripts/level_editor.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+from cryptography.fernet import Fernet
+import os
 
 pygame.init()
 
@@ -13,16 +15,68 @@ red = (255, 0, 0)
 gray = (128, 128, 128)
 
 walls = []
+mid_points = []
+to_delete = None
 
 def draw_walls():
-    for wall in walls:
-        pygame.draw.line(screen, black, wall[:2], wall[2:], 2)
+    for idx, wall in enumerate(walls):
+        if type(wall) == tuple:
+            pygame.draw.line(screen, black if idx != to_delete else red, wall[:2], wall[2:], 2)
 
 def draw_preview_line(start_pos, end_pos):
     pygame.draw.line(screen, gray, start_pos, end_pos, 2)
 
-def delete_wall(pos):
-    pass
+def get_key():
+    key_path = os.path.join(os.path.dirname(__file__)[:-7], "configs/", "key.key")
+    try:
+        with open(key_path, 'rb') as key_file:
+            key = key_file.read()
+            return key
+    except FileNotFoundError:
+        key = Fernet.generate_key()
+        with open(key_path, 'wb') as key_file:
+            key_file.write(key)
+        return key
+    
+def encrypt_data(data):
+    return cipher_suite.encrypt(data.encode())
+
+def decrypt_data(encrypted_data):
+    return cipher_suite.decrypt(encrypted_data).decode()
+
+def write_encrypted_data_to_file():
+    with open(os.path.join(os.path.dirname(__file__), "levels/", "main.elvl"), "wb") as f:
+        for wall in walls:
+            encrypted_data = encrypt_data(','.join([f'{wall[0]},{wall[1]},{wall[2]},{wall[3]}' for wall in walls]))
+            f.write(encrypted_data)
+
+def read_encrypted_data_from_file():
+    with open(os.path.join(os.path.dirname(__file__), "levels/", "main.elvl"), "rb") as f:
+        walls.clear()
+        encrypted_data = f.read()
+        decrypted_data = decrypt_data(encrypted_data)
+
+        wall_data = decrypted_data.split(',')
+        walls_data = [tuple(map(int, wall_data[i:i+4])) for i in range(0, len(wall_data), 4)]
+        
+        walls.extend(walls_data)
+
+key = get_key()
+cipher_suite = Fernet(key)
+
+def generate_mid_points():
+    mid_points.clear()
+    for wall in walls:
+        start_pos = wall[:2]
+        end_pos = wall[2:]
+        mid_points.append(((start_pos[0] + end_pos[0]) // 2, (start_pos[1] + end_pos[1]) // 2))
+
+def select_wall(pos):
+    global to_delete
+    for mid_point in mid_points:
+        if distance(pos, mid_point) < 100:
+            to_delete = mid_points.index(mid_point)
+            return
 
 def snap_to_point(pos):
     for wall in walls:
@@ -43,6 +97,7 @@ start_pos = None
 
 while running:
     screen.fill(white)
+    generate_mid_points()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -57,7 +112,20 @@ while running:
                     walls.append((*start_pos, *end_pos))
                     drawing = False
             elif event.button == 3:  
-                delete_wall(event.pos)
+                select_wall(event.pos)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DELETE:
+                if to_delete != None:
+                    del walls[to_delete]
+                    to_delete = None
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
+            if event.key == pygame.K_s:
+                write_encrypted_data_to_file()
+
+            if event.key == pygame.K_l:
+                read_encrypted_data_from_file()
 
     if drawing:
         draw_preview_line(start_pos, snap_to_point(pygame.mouse.get_pos()))
